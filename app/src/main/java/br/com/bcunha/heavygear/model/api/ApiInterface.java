d1f8f8d536ef29fcc9b2384d52ce0b042c6aea90
Bruno Cunha <sc.bruno@hotmail.com.br>
2016-10-17 01:09:33 -0200

    Atualiza APIClient

---
+++ app/src/main/java/br/com/bcunha/heavygear/model/api/ApiInterface.java
@@ -0,0 +1,20 @@
+package br.com.bcunha.heavygear.model.api;
+
+import br.com.bcunha.heavygear.model.pojo.RespostaInfo;
+import br.com.bcunha.heavygear.model.pojo.RespostaValor;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by BRUNO on 09/10/2016.
+ */
+
+public interface ApiInterface {
+
+    //GET("yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22{codigo}.SA%22)&env=store://datatables.org/alltableswithkeys&format=json")
+    @GET("yql")
+    Call<RespostaValor> getQueryValor (@retrofit2.http.Query("q") String query, @retrofit2.http.Query("env") String env, @retrofit2.http.Query("format") String format);
+
+    @GET("yql")
+    Call<RespostaInfo> getQueryInfo (@retrofit2.http.Query("q") String query, @retrofit2.http.Query("env") String env, @retrofit2.http.Query("format") String format);
+}
--- app/src/main/java/br/com/bcunha/heavygear/ui/activities/HeavyGearActivity.java
+++ app/src/main/java/br/com/bcunha/heavygear/ui/activities/HeavyGearActivity.java
@@ -8,9 +8,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import br.com.bcunha.heavygear.R;
-import br.com.bcunha.heavygear.model.api.Api;
-import br.com.bcunha.heavygear.model.pojo.Resposta;
-import br.com.bcunha.heavygear.model.pojo.Resposta;
+import br.com.bcunha.heavygear.model.api.ApiClient;
+import br.com.bcunha.heavygear.model.api.ApiInterface;
+import br.com.bcunha.heavygear.model.pojo.RespostaInfo;
+import br.com.bcunha.heavygear.model.pojo.RespostaValor;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -18,9 +19,11 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static br.com.bcunha.heavygear.model.api.ApiClient.getRetrofit;
+
 public class HeavyGearActivity extends Activity {
 
-    private Api apiService;
+    private ApiInterface apiClient;
 
     @BindView(R.id.edtCodigo)
     EditText edtCotacao;
@@ -36,25 +39,42 @@ public class HeavyGearActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heavy_gear);
 
-        apiService = Api.getInstance();
+        apiClient = ApiClient.getRetrofit().create(ApiInterface.class);
         ButterKnife.bind(this);
     }
 
     @OnClick(R.id.btnCotacao)
     public void buscar(View view) {
-
-        apiService.getCotacao(edtCotacao.getText().toString()).enqueue(new Callback<Resposta>(){
+        apiClient.getQueryValor(
+            ApiClient.QUERY_QUOTE.replace("?codigo?", edtCotacao.getText().toString()),
+            ApiClient.ENV,
+            ApiClient.FORMAT)
+        .enqueue(new Callback<RespostaValor>(){
             @Override
-            public void onResponse(Call<Resposta> call, Response<Resposta> response) {
-                int statusCode = response.code();
-                Resposta resposta = response.body();
-                txtCotacao.setText(resposta.getQuery().getResults().getQuote().getLastTradePriceOnly().toString());
+            public void onResponse(Call<RespostaValor> call, Response<RespostaValor> response) {
+                RespostaValor.Quote  respostaValor =  response.body().getQuery().getResults().getQuote();
+                txtCotacao.setText(respostaValor.getLastTradePriceOnly().toString());
             }
-
             @Override
-            public void onFailure(Call<Resposta> call, Throwable t) {
+            public void onFailure(Call<RespostaValor> call, Throwable t) {
                 // Log error here since request failed
             }
         });
+        /*apiClient.getQueryInfo(
+            ApiClient.QUERY_QUOTES.replace("?codigo?", edtCotacao.getText().toString()),
+            ApiClient.ENV,
+            ApiClient.FORMAT)
+        .enqueue(new Callback<RespostaInfo>(){
+            @Override
+            public void onResponse(Call<RespostaInfo> call, Response<RespostaInfo> response) {
+                response.body();
+                RespostaInfo.Quote respostaInfo = response.body().getQuery().getResults().getQuote();
+                //txtCotacao.setText(Double.toString(respostaInfo.getLastTradePriceOnly()));
+            }
+            @Override
+            public void onFailure(Call<RespostaInfo> call, Throwable t) {
+                // Log error here since request failed
+            }
+        });*/
     }
 }
