847575ddc02e86a566ff0d60c1794bf189c04362
bruno-cunha <sc.bruno@hotmail.com.br>
2017-02-28 05:09:27 -0300

    Adicionar Consulta API pelo Servico

--- app/src/main/java/br/com/bcunha/heavygear/model/service/HeavyService.java
+++ app/src/main/java/br/com/bcunha/heavygear/model/service/HeavyService.java
@@ -8,23 +8,28 @@ import android.util.Log;
 import android.view.Window;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.bcunha.heavygear.model.api.ApiClient;
 import br.com.bcunha.heavygear.model.api.ApiInterface;
 import br.com.bcunha.heavygear.model.pojo.Acao;
 import br.com.bcunha.heavygear.model.pojo.RespostaSimplesMultipla;
+import br.com.bcunha.heavygear.ui.adapters.RvAdapter;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import static br.com.bcunha.heavygear.R.id.recyclerView;
 
 public class HeavyService extends Service {
 
     private String LOG_TAG = "HeavyService";
     private HeavyBinder mBinder = new HeavyBinder();
-    //private ApiInterface apiClient;
+    private ApiInterface apiClient;
     private Worker worker;
-    public List<Acao> watchlist;
+    private List<Acao> watchList;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -45,7 +50,21 @@ public class HeavyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        //apiClient = ApiClient.getRetrofit().create(ApiInterface.class);
+        apiClient = ApiClient.getRetrofit().create(ApiInterface.class);
+
+        watchList = new ArrayList<Acao>();
+        Acao acaoBRFS3 = new Acao("BRFS3.SA", "Brasil Foodas S.A" , "",  51.11, true);
+        Acao acaoITSA4 = new Acao("ITSA4.SA", "Itau SA"           , "",  13.11, true);
+        Acao acaoGGBR3 = new Acao("GGBR4.SA", "Gerdau"            , "",  13.11, true);
+        Acao acaoGOAU4 = new Acao("GOAU4.SA", "Metalurgica Gerdau", "",  13.11, true);
+        Acao acaoJBSS3 = new Acao("JBSS3.SA", "JBS"               , "",  13.11, true);
+
+        watchList.add(acaoBRFS3);
+        watchList.add(acaoITSA4);
+        watchList.add(acaoGGBR3);
+        watchList.add(acaoGOAU4);
+        watchList.add(acaoJBSS3);
+
         worker = new Worker();
         Log.i(LOG_TAG, "onCreate");
     }
@@ -65,36 +84,34 @@ public class HeavyService extends Service {
         @Override
         public void run() {
             while (ativo && count < 100) {
+                apiClient.getQueryValorLista(
+                ApiClient.QUERY_QUOTE_LISTA.replace("?codigo?", formatCodigo(watchList)),
+                ApiClient.ENV,
+                ApiClient.FORMAT)
+                .enqueue(new Callback<RespostaSimplesMultipla>() {
+                    @Override
+                    public void onResponse(Call<RespostaSimplesMultipla> call,
+                                           Response<RespostaSimplesMultipla> response) {
+//                      rvAdapter = new RvAdapter().createFromQuote(response.body().getQuery().getResults().getQuote());
+//                      recyclerView.setAdapter(rvAdapter);
+                        Log.i(LOG_TAG, "Manipulando o resultado:" + response.body().getQuery().getResults().getQuote().get(1).getLastTradePriceOnly());
+                    }
+
+                    @Override
+                    public void onFailure(Call<RespostaSimplesMultipla> call, Throwable t) {
+                        Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 count++;
-                Log.i(LOG_TAG, "Contador"+count);
+                Log.i(LOG_TAG, "Consulta Executada");
             }
             stopSelf();
-//            while (ativo && count < 100) {
-//                apiClient.getQueryValorLista(
-//                ApiClient.QUERY_QUOTE_LISTA.replace("?codigo?", formatCodigo(watchlist)),
-//                ApiClient.ENV,
-//                ApiClient.FORMAT)
-//                .enqueue(new Callback<RespostaSimplesMultipla>() {
-//                    @Override
-//                    public void onResponse(Call<RespostaSimplesMultipla> call,
-//                                           Response<RespostaSimplesMultipla> response) {
-////                      rvAdapter = new RvAdapter().createFromQuote(response.body().getQuery().getResults().getQuote());
-////                      recyclerView.setAdapter(rvAdapter);
-//                        minhaString();
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<RespostaSimplesMultipla> call, Throwable t) {
-////                    Toast.ma7keText(getApplicationContext(), "Failure", Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//                Log.i(LOG_TAG, "Executa enqueue");
-//            }
         }
     }
 
